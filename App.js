0a14b53e1f1ec70e6106f373d451fff09ca63a04
gwimer84 <gwimer84@gmail.com>
2023-08-30 21:45:15 -0400

    almost done with notes app

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View,TextInput } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Today's Tasks */}
+      <View style = {styles.tasksWrapper}>
+        <Text style = {styles.sectionTitle}> Today's tasks </Text>
+        <View style = {styles.items}>
+        {/* This is where the tasks will go! */}
+        <Task text = {'Task1'} />
+        <Task text = {'Task2'}/>
+        <Task />
+        <Task />
+        <Task />
+        </View>
+      </View>
+
+
+      <View style={styles.inputWrapper}>
+        <TextInput style={styles.input} placeholder={'Write a task'} />
+      </View>
     </View>
+
+    
+
+
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: '#E8EAED',
+    },
+    tasksWrapper:{
+      paddingTop: 15,
+      paddingHorizontal: 20,
+    },
+    sectionTitle: {
+      fontSize: 24,
+      fontWeight: 'bold'
+
+    }, 
+    items: {
+      marginTop:30,
+    }, 
+    input: {
+      paddingVertical: 15,
+      paddingHorizontal: 15,
+      backgroundColor: '#FFF',
+      borderRadius:60,
+      borderColor: '#C0C0C0',
+      borderWidth: 1,
+      width: 250,
+
+    },
 });
